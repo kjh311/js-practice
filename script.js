63298d23af33b7a1204dc16ea92fef2eecca16af
Kevin Huelsmann <kjh311@gmail.com>
2016-10-17 14:07:29 -0700

    jquery2

--- script.js
+++ script.js
@@ -5,4 +5,10 @@ $( document ).ready(function() {
 $('#button2').click(function(){
   $('#jquery-div').load("jquery.txt");
   });
+
+$("#button3").click(function(){
+  $.ajax({url: "jquery2.txt", success: function(result){
+        $("#jquery-2").html(result);
+    }});
+});
 });
