6ba49918a6eb78c0df812c1cb2d88326d6e07af3
Kevin Huelsmann <kjh311@gmail.com>
2016-10-17 14:03:55 -0700

    jquery

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 $( document ).ready(function() {
 
-function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "text.txt", true);
-  xhttp.send();
-}
+
 
 $('#button2').click(function(){
   $('#jquery-div').load("jquery.txt");
